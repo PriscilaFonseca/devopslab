c4c7ba082f99c0ecbc812ec7deb2ef257797e43f
Pri <69484180+PriscilaFonseca@users.noreply.github.com>
2022-02-01 19:08:30 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Priscila"
+    return "Hello World - Priscila - FIAP"
 
 if __name__ == '__main__':
     app.run()
